cb57b628f2cb04916dbe62b86378a176dd33d7ce
geronilton <geronilton001@gmail.com>
2024-11-10 20:04:43 -0300

    att

--- cad.php
+++ cad.php
@@ -1,20 +1,34 @@
-    <?php
-        include 'connectionDB.php';
+<?php
+include 'connectionDB.php';
 
-        $nome = $_REQUEST["nome"];
-        $email = $_REQUEST["email"];
-        $cpf = $_REQUEST["cpf"];
-        $dataFiliacao = $_REQUEST["data_filiacao"];
+$nome = $_REQUEST["nome"];
+$email = $_REQUEST["email"];
+$cpf = preg_replace('/[^0-9]/', '', $_POST['cpf']);
+$dataFiliacao = $_REQUEST["data_filiacao"];
 
-        $sql = "INSERT INTO associado (nome, email, cpf, data_filiacao) VALUES ('$nome', '$email', '$cpf', '$dataFiliacao')";
 
-        if ($conn->query($sql) === TRUE) {
-            echo "Novo registro inserido com sucesso!";
-            header("Location: listarAssociado.php");
-        } else {
-            echo "Erro: " . $sql . "<br>" . $conn->error;
-        }
-        
-        $conn->close();
+$sql = "INSERT INTO associado (nome, email, cpf, data_filiacao) VALUES ('$nome', '$email', '$cpf', '$dataFiliacao')";
 
-    ?>
+if ($conn->query($sql) === TRUE) {
+    $id_associado = $conn->insert_id;
+
+    
+    $status_pagamento = 0; 
+    $tituloAnuidade = "Anuidade Recorrente";
+    $dataVencimento = date('Y-m-d', strtotime('+1 month', strtotime($dataFiliacao)));
+    
+    $sqlAnuidade = "INSERT INTO anuidade (titulo, data_vencimento, status_pagamento, id_associado) VALUES ('$tituloAnuidade', '$dataVencimento', '$status_pagamento', '$id_associado')";
+
+    if ($conn->query($sqlAnuidade) === TRUE) {
+        echo "Novo registro inserido com sucesso!";
+        header("Location: index.php");
+        exit();
+    } else {
+        echo "Erro ao inserir anuidade: " . $conn->error;
+    }
+} else {
+    echo "Erro: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
